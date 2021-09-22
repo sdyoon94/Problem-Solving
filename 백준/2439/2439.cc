af900f76eeafc3d1839ac76793735594a8c73b34
sdyoon94 <sdyoon94@gmail.com>
2021-09-23 04:40:36 +0900

    Update 2439.cc

--- 백준/2439/2439.cc
+++ 백준/2439/2439.cc
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
-
+    int N;
+    scanf("%d", &N);
+    for(int i=1;i<=N;i++){
+        for(int j=N-i;j>0;j--) printf(" ");
+        for(int j=0;j<i;j++) printf("*");
+        printf("\n");
+    }
     return 0;
 }

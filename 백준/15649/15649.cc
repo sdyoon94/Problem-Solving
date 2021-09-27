dde56653db8a6b90effef835f0f8661ed22c0c76
sdyoon94 <sdyoon94@gmail.com>
2021-09-28 01:35:36 +0900

    15649

---
+++ 백준/15649/15649.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bt(int N, int M, int cur, vector<int> seq, vector<bool> chk){
+    if(M==cur){
+        for(int i=0; i<M; i++){
+            printf("%d ", seq[i]);
+        }
+        printf("\n");
+        return;
+    }
+    else{
+        for(int i=1; i<=N; i++){
+            if(!chk[i]){
+                chk[i]=true;
+                seq[cur]=i;
+                bt(N, M, cur+1, seq, chk);
+                chk[i]=false;
+            }
+        }
+    }
+}
+
+int main(){
+    int N, M;
+    scanf("%d %d", &N, &M);
+    vector<int> seq(M, 0);
+    vector<bool> chk(N+1, 0);
+    bt(N, M, 0, seq, chk);
+    return 0;
+}

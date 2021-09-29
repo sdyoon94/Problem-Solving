9282d9baaf72fa6d3576e47fbfbbfc432181d75d
sdyoon94 <sdyoon94@gmail.com>
2021-09-30 00:24:04 +0900

    11050

--- 백준/11050/11050.cc
+++ 백준/11050/11050.cc
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-void comb(int a, int b){
-    if(b>a/2) b=a-b;
-    int c=1, d=1;
-    for(int i=1; i<=b; i++){
-        c*=a+1-i;
-        d*=i;
-    }
-    printf("%d\n", c/d);
-}
-
 int main(){
     int N, K; scanf("%d %d", &N, &K);
-    comb(N, K);
+    int pas[1001][1001];
+    memset(pas, 0, sizeof(int) * 1001 * 1001);
+    for(int i=0; i<=1000; i++) pas[i][0]=1;
+    for(int i=1; i<=1000; i++){
+        for(int j=1; j<=i; j++){
+            pas[i][j]=pas[i-1][j-1]+pas[i-1][j];
+            pas[i][j]%=10007;
+        }
+    }
+    printf("%d\n", pas[N][K]);
     return 0;
 }

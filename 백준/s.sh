1ae983e121a7aedb08fcc0a46eedbc06856f8a03
sdyoon94 <sdyoon94@gmail.com>
2021-10-10 00:38:46 +0900

    1927

---
+++ 백준/s.sh
@@ -0,0 +1,13 @@
+# !sh
+mkdir $1
+echo "#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}" > $1/$1.cc

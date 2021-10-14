665f7ef1674753cf8bb2785cbcb9c78df8b89b51
윤성도 <sdyoon94@gmail.com>
2021-10-15 00:21:05 +0900

    Update 13458.cc

--- 백준/13458/13458.cc
+++ 백준/13458/13458.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
 
@@ -7,12 +8,12 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N; cin >> N;
-    vector<int> A(N);
-    for(int i=0; i<N; i++) cin >> A[i];
-    int B, C; cin >> B >> C;
-    int ans=0;
-    for(int i=0; i<N; i++){
+    ll N; cin >> N;
+    vector<ll> A(N);
+    for(ll i=0; i<N; i++) cin >> A[i];
+    ll B, C; cin >> B >> C;
+    ll ans=0;
+    for(ll i=0; i<N; i++){
         ans++;
         A[i]-=B;
         if(A[i]>0){

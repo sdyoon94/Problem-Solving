b22b3559e264c3b9ecec28c9ebb1624ead4ec39f
윤성도 <90303354+sdyoon94@users.noreply.github.com>
2022-09-21 02:16:56 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11240 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) throws Exception{
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}

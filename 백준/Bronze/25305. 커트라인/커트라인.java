30d2ddb9eb9f611d1ab4444772284178c62a20e6
윤성도 <90303354+sdyoon94@users.noreply.github.com>
2022-09-21 23:56:01 +0900

    [Bronze II] Title: 커트라인, Time: 228 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (a, b) -> b - a);
+        System.out.println(arr[k - 1]);
+    }
+}
